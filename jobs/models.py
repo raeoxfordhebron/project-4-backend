e1ff91e277cf66cbee6afb6ba438bae24e1bea56
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-18 14:20:50 -0500

    makemigrations and migrations are complete to create the job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db import models 
 
 # Create your models here.
+
+class Job(models.Model):
+    jobid = models.IntegerField()
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    type = models.CharField(max_length=50)
+    salary = models.CharField(max_length=50)
+    remote = models.BooleanField()
+    
+class Language(models.Model):
+    languageid = models.IntegerField()
+    name = models.CharField(max_length=50)
+    jobid = models.IntegerField()
