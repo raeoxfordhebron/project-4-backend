4c2b2fd77b7d25b79545b009b7eeeed170ae0c18
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-22 21:32:45 -0500

    remove jobid from Job model

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Job(models.Model):
-    jobid = models.IntegerField()
     title = models.CharField(max_length=50)
     description = models.TextField()
     type = models.CharField(max_length=50)
